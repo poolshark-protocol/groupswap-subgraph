4fb8733ea2884b17d15c082205cb6eb30ad1a590
alphak3y <alphamik80@gmail.com>
2021-12-04 21:51:15 -0600

    updated abi for indexed values

--- tests/GroupSwap/handleDeposit.test.ts
+++ tests/GroupSwap/handleDeposit.test.ts
@@ -29,9 +29,27 @@ test("Can mock and call function with different argument types", () => {
         .returns([ethereum.Value.fromString("result")])
     let val = ethereum.call(new ethereum.SmartContractCall("conName", Address.fromString("0x90cBa2Bbb19ecc291A12066Fd8329D65FA1f1947"), "funcName", "funcName():(void)", argsArray))![0]
     
-    //assert.equals(ethereum.Value.fromString("result"), val)
-})    
+    assert.equals(ethereum.Value.fromString("result"), val)
+})
 
-    // test("handleDepositedToGroup - should handle new userEntity", () => {
-        
-    // })
+test("Can test if mocked function reverts", () => {
+    createMockedFunction(Address.fromString("0x89205A3A3b2A69De6Dbf7f01ED13B2108B2c43e7"), "revert", "").reverts()
+    let val = ethereum.call(new ethereum.SmartContractCall("conName", Address.fromString("0x89205A3A3b2A69De6Dbf7f01ED13B2108B2c43e7"), "revert", "", []))
+  
+    assert.equals(ethereum.Value.fromBoolean(true), ethereum.Value.fromBoolean(val == null))
+})
+
+test("handleDepositedToGroup - should handle new userEntity", () => {
+    let contractAddress = Address.fromString("0x89205A3A3b2A69De6Dbf7f01ED13B2108B2c43e7")
+    let expectedResult = Address.fromString("0x90cBa2Bbb19ecc291A12066Fd8329D65FA1f1947")
+    let groupIdParam = BigInt.fromString("1234")
+    createMockedFunction(contractAddress, "deposit", "gravatarToOwner(uint256):(address)")
+      .withArgs([ethereum.Value.fromSignedBigInt(bigIntParam)])
+      .returns([ethereum.Value.fromAddress(Address.fromString("0x90cBa2Bbb19ecc291A12066Fd8329D65FA1f1947"))])
+  
+    let gravity = Gravity.bind(contractAddress)
+    let result = gravity.gravatarToOwner(bigIntParam)
+  
+    assert.equals(ethereum.Value.fromAddress(expectedResult), ethereum.Value.fromAddress(result))
+})
+
