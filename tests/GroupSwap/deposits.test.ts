5b1d9aed675e914d17047cd4d562f02afe778acd
alphak3y <alphamik80@gmail.com>
2021-12-06 15:55:21 -0600

    add new groupEntity tests

--- tests/GroupSwap/deposits.test.ts
+++ tests/GroupSwap/deposits.test.ts
@@ -15,13 +15,19 @@ test("Can initialise store with an array of Entity objects", () => {
   let deposit = new GroupData("0xe46f9cbe5d8c6d3c9df0fa21d0d8c906b17c3346d5af27bd6e59913321162a6e")
   deposit.save()
 
-  assert.fieldEquals(GROUPDATA_ENTITY_TYPE, 0xe46f9cbe5d8c6d3c9df0fa21d0d8c906b17c3346d5af27bd6e59913321162a6e,
+  assert.fieldEquals(GROUPDATA_ENTITY_TYPE, "0xe46f9cbe5d8c6d3c9df0fa21d0d8c906b17c3346d5af27bd6e59913321162a6e",
+                                            "id",
+                                            "0xe46f9cbe5d8c6d3c9df0fa21d0d8c906b17c3346d5af27bd6e59913321162a6e")
+
+  assert.fieldEquals(GROUPDATA_ENTITY_TYPE, "0xe46f9cbe5d8c6d3c9df0fa21d0d8c906b17c3346d5af27bd6e59913321162a6e",
                                             "groupAmount",
-                                            Value.fromBigInt(BigInt.zero()).toString())
+                                            "0"
+  )
 
   assert.fieldEquals(GROUPDATA_ENTITY_TYPE, "0xe46f9cbe5d8c6d3c9df0fa21d0d8c906b17c3346d5af27bd6e59913321162a6e",
                                             "groupGwei",
-                                            Value.fromBigInt(BigInt.zero()).toString())
+                                            "0"
+  )
 
   clearStore()
 })
@@ -57,6 +63,47 @@ test("handleDepositedToGroup - should handle new groupEntity", () => {
     "0xe46f9cbe5d8c6d3c9df0fa21d0d8c906b17c3346d5af27bd6e59913321162a6e",
     "groupGwei",
     "2"
+  )
+
+  clearStore()
+})
+
+test("handleDepositedToGroup - should handle existing groupEntity", () => {
+
+  let deposit = new GroupData("0xe46f9cbe5d8c6d3c9df0fa21d0d8c906b17c3346d5af27bd6e59913321162a6e")
+  deposit.groupAmount = BigInt.fromI32(2)
+  deposit.groupGwei   = BigInt.fromI32(3)
+  deposit.save()
+
+  // Call mappings
+  let newDepositedToGroupEvent = createNewDepositedToGroupEvent(
+      Bytes.fromHexString("0xe46f9cbe5d8c6d3c9df0fa21d0d8c906b17c3346d5af27bd6e59913321162a6e"),
+      "0x89205A3A3b2A69De6Dbf7f01ED13B2108B2c43e7",
+      BigInt.fromI32(1),
+      BigInt.fromI32(2),
+  )
+
+  handleNewDepositedToGroups([newDepositedToGroupEvent])
+
+  assert.fieldEquals(
+      GROUPDATA_ENTITY_TYPE,
+      "0xe46f9cbe5d8c6d3c9df0fa21d0d8c906b17c3346d5af27bd6e59913321162a6e",
+      "id",
+      "0xe46f9cbe5d8c6d3c9df0fa21d0d8c906b17c3346d5af27bd6e59913321162a6e"
+  )
+
+  assert.fieldEquals(
+    GROUPDATA_ENTITY_TYPE,
+    "0xe46f9cbe5d8c6d3c9df0fa21d0d8c906b17c3346d5af27bd6e59913321162a6e",
+    "groupAmount",
+    "3"
+  )
+
+  assert.fieldEquals(
+    GROUPDATA_ENTITY_TYPE,
+    "0xe46f9cbe5d8c6d3c9df0fa21d0d8c906b17c3346d5af27bd6e59913321162a6e",
+    "groupGwei",
+    "5"
   )
 
   clearStore()
